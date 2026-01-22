ff25e5feb75989d86da9e7560d43b5bb4f63462b
Yamankassem <ykassem957@gmail.com>
2026-01-22 18:09:56 +0300

    feat(auth): add Sanctum authentication and RBAC with Spatie permissions

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -2,55 +2,39 @@
 
 namespace Modules\Auth\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Modules\Auth\Services\AuthService;
+use Modules\Auth\Http\Requests\LoginRequest;
+use Modules\Auth\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct(private AuthService $auth) {}
+
+    public function register(RegisterRequest $request)
     {
-        return view('auth::index');
+        $result = $this->auth->register($request->validated());
+
+        return response()->json($result, 201);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function login(LoginRequest $request)
     {
-        return view('auth::create');
+        $result = $this->auth->login($request->validated());
+
+        return response()->json($result, 201);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {}
-
-    /**
-     * Show the specified resource.
-     */
-    public function show($id)
+    public function me(Request $request)
     {
-        return view('auth::show');
+         return response()->json(['user' => $request->user()], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
+    public function logout(Request $request)
     {
-        return view('auth::edit');
+        $this->auth->logoutCurrent($request->user());
+
+        return response()->json(['ok' => true], 200);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
 }
